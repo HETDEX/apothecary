bb0f389022748391b9d6fa7923e15fddd069cc39
phantomamoeba <dustin.davis@utexas.edu>
2025-08-06 08:55:47 -0500

    single shot reduction wrapper, in progress --add local_script_repo --add check of guiders for FWHM to use when fewer than 3 exposures --working on diagnose and elixer

--- single_shot_reduction/local_script_repo/vdrp/vdrp/src/vdrp/__init__.py
+++
@@ -1,4 +0,0 @@
-import pkg_resources as pkgr
-
-__version__ = pkgr.get_distribution('vdrp').version
-
--- single_shot_reduction/local_script_repo/vdrp/vdrp/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# . ../setup.sh
-
-# python -m unittest vdrp.tests.test_cltools vdrp.tests.test_daophot  vdrp.tests.test_astrometry
-
-cd unittest
-python runtests.py
