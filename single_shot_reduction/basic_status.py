f95248c40ac804d4b2c060d4d35fc41e0319e722
phantomamoeba <dustin.davis@utexas.edu>
2025-10-20 16:17:00 -0500

    add number of line and cont detections

--- single_shot_reduction/basic_status.py
+++ single_shot_reduction/basic_status.py
@@ -10,11 +10,32 @@ import glob
 import tables
 import traceback
 import numpy as np
+import os
 
 
 h5_fns = sorted(glob.glob("sci*/20*v???.h5"))
-print(f"#shotid\t\tra\t\tdec\t\texp\ttime\tfwhm\ttput\tdither")
+print(f"#shotid\t\tra\t\tdec\t\texp\ttime\tfwhm\ttput\tline_ct\tcont_ct\tdither")
 for fn in h5_fns:
+    cont_ct = -1
+    line_ct = -1
+
+    try:
+        sci = fn.split("/")[0]
+
+        det_fn = os.path.join(sci,"elixer/line.dets")
+        if os.path.exists(det_fn):
+            with open(det_fn,'r') as f:
+                lines = f.read()
+                line_ct = lines.count("\n")
+
+        det_fn = os.path.join(sci,"elixer/cont.dets")
+        if os.path.exists(det_fn):
+            with open(det_fn,'r') as f:
+                lines = f.read()
+                cont_ct = lines.count("\n")
+
+    except:
+        print(f"[{fn}] Exception (trying wc):", traceback.format_exc())
 
     try:
         h5 = tables.open_file(fn,mode="r")
@@ -29,7 +50,7 @@ for fn in h5_fns:
         dither = [(x,y) for x,y in zip(xs,ys)]
 
         print(f"{row['shotid']}\t{row['ra']:0.6f}\t{row['dec']:0.6f}\t{num_exp}\t{total_time:0.1f}\t"
-              f"{row['fwhm_virus']:0.2f}\t{row['response_4540']:0.2f}\t{dither}")
+              f"{row['fwhm_virus']:0.2f}\t{row['response_4540']:0.2f}\t{line_ct}\t{cont_ct}\t{dither}")
 
         h5.close()
     except:
