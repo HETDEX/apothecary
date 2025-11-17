00da3c6881ea8aff55404cd5cc3ce231b5d60fb3
phantomamoeba <dustin.davis@utexas.edu>
2025-11-16 20:41:01 -0600

    example db_insert_yymm.sh

---
+++ single_shot_reduction/db_insert_yymm.sh
@@ -0,0 +1,2 @@
+set -o noglob ; python3 make_report_db.py --db_name /scratch/03261/polonius/parallel/detect/image_db/elixer_reports_YYMM.db --img_dir /scratch/03261/polonius/parallel/all_pngs_YYYYMM --img_name YYMM*[0-9].png &
+set -o noglob ; python3 make_report_db.py --db_name /scratch/03261/polonius/parallel/detect/image_db/elixer_reports_YYMM_nei.db --img_dir /scratch/03261/polonius/parallel/all_pngs_YYYYMM --img_name YYMM*_nei.png &
